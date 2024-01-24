d8bb27c0fc0be90fb4f4f31f22852e70e91e3cf7
tottopyou <rgrabovsky2006@gmail.com>
2024-01-24 16:49:18 +0100

    Heroku

--- config.py
+++ config.py
@@ -2,12 +2,12 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'biba and boba'
+    SECRET_KEY = os.environ['SECRET_KEY']
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    MAIL_SERVER = 'smtp.gmail.com'
-    MAIL_PORT = 587    
-    MAIL_USE_TLS = True
-    MAIL_USERNAME = ''
-    MAIL_PASSWORD = ''
-    ADMINS = ['']
+    MAIL_SERVER = os.environ['MAIL_SERVER']
+    MAIL_PORT = os.environ['MAIL_PORT']    
+    MAIL_USE_TLS = os.environ['MAIL_USE_TLS']
+    MAIL_USERNAME = os.environ['MAIL_USERNAME']
+    MAIL_PASSWORD = os.environ['MAIL_PASSWORD']
+    ADMINS = os.environ['ADMINS']
